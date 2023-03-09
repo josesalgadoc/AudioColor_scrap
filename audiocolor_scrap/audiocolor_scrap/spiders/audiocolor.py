8276c4ebdf5d60847f9f8b0dbdb95ab68c301af6
Jose Salgado <josesalgadoc98@gmail.com>
2023-03-09 17:53:20 -0500

    Start with Selenium

--- audiocolor_scrap/audiocolor_scrap/spiders/audiocolor.py
+++ audiocolor_scrap/audiocolor_scrap/spiders/audiocolor.py
@@ -1,52 +1,51 @@
 import scrapy
 
 class ProductsAudiocolor(scrapy.Spider):
-    name = "products"
+    name = "audiocolor"
     start_urls = [
-        "https://www.falabella.com.co/falabella-co/category/cat3050947/SmartWatch"
-        # "https://www.audiocolor.co/categoria/gamer"
+        "https://www.audiocolor.co/categoria/gamer/"
         
     ]
 
     custom_settings = {
         'FEEDS': {
-            'gamer.json': {
+            'audiocolor_gamer.json': {
                 'format': 'json',
                 'encoding': 'utf8'
             },
         },
-        'CONCURRENT_REQUEST': 24, 
-        'MEMUSAGE_LIMIT_MB': 2048,
+        # 'CONCURRENT_REQUEST': 24, 
+        # 'MEMUSAGE_LIMIT_MB': 2048,
         'USER_AGENT': 'Copi',
         'ROBOTSTXT_OBE': True  
     }
     
-    # title = '//div[@class="product-description"]/h3/a/text()'
-    
-    # test = response.xpath('//div[@class="elementor-inner"]//div[@class="elementor-widget-wrap"]//div[@class="elementor-widget-cotainer"]/h3[@class="jet-woo-builder-archive-product-title"]/a/@href').extract()
-    
-    # accesories = response.xpath('//div[@class="elementor-inner"]//div[@class="elementor-container elementor-column-gap-default"]//div[@class="jet-listing-grid jet-listing"]//div[@class="slick-list draggable"]//div[@class="elementor-widget-container"]/h2/text()').extract()
-    
-    # ktronix = response.xpath('//div[@cclass="plp__result-slot plp__result-slot--list"]//div[@class="ais-InfiniteHits product__listing"]//li[@class="ais-InfiniteHits-item product__item js-product-item js-algolia-product-click"]/h3[@class="product__item__top__title"]/a/text()').extract()
-    
-    # ktronix = response.xpath('//div[@class="plp__result-slot plp__result-slot--list"]//div[@class="ais-InfiniteHits product__listing"]//li[@class="ais-InfiniteHits-item product__item js-product-item js-algolia-product-click"]/h3/a/text()').extract()
-
-    # falabella_smartwatch = '//div[@class="jsx-1250940326 pod-group--container container"]//div[@id="testId-searchResults-products" and @class="jsx-4099777552 search-results--products"]//div[@pod-layout="4_GRID" and @class="jsx-4001457643 search-results-4-grid grid-pod"]//a/@href'
-
-    falabella_title = '//div[@class="Header-module_header-util-bar__1Qt0E"]//div[@class="MediaComponent-module_tablet-desktop__3xCIl"]//span[@class="HamburgerBtn-module_title__26h1r"]/text()'
+    title_article_link = '//h3[@class="jet-woo-builder-archive-product-title"]/a/@href'
+    cargar_mas_button = '//div[@class="elementor-button-wrapper"]/a/@href'
 
     def parse(self, response):
-        falabella = response.xpath(self.falabella_title).extract()
-
-        # print('*' * 100)
-        # self.logger.info('Response status: %s' % response.status)
-        # print('*' * 100)
-
-        print('*' * 100)
-        self.logger.info('Response content: %s' % response.body)
-        print('*' * 100)
+        title_article_link = response.xpath(self.title_article_link).extract()
+        cargar_mas_button_link = response.xpath(self.cargar_mas_button).extract_first()
 
         yield {
-            'title': falabella
-        }
-    
+            'title_article_link': title_article_link
+            }
+
+        if cargar_mas_button_link:
+            yield response.follow(cargar_mas_button_link, callback=self.parse_articles, cb_kwargs={'title_article_link': title_article_link})
+
+    def parse_articles(self, response, **kwargs):
+        if kwargs:
+            title_article_link = kwargs['title_article_link']
+        
+        title_article_link.extend(response.xpath(self.title_article_link).extract())
+
+        cargar_mas_button_link = response.xpath(self.cargar_mas_button).extract_first()
+
+        if cargar_mas_button_link:
+            yield response.follow(cargar_mas_button_link, callback=self.parse_articles, cb_kwargs={'title_article_link': title_article_link})
+        else:
+            yield {
+                'title_article_link': title_article_link
+            }
+
